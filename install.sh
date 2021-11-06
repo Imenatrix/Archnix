7d6bbc1d524163976cf42d6dd61b604378eb10cb
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-11-06 14:18:35 -0300

    Fix chroot commands

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ mkdir /mnt/efi
 mount /dev/sda1 /mnt/efi
 pacstrap /mnt base linux linux-firmware dhcp
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
+arch-chroot /mnt << 'EOF'
 ln -sf /usr/share/zoneinfo/Brazil/East /etc/localtime
 hwclock --systohc
 echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
@@ -16,3 +16,4 @@ passwd
 pacman -S grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
+EOF
