7c7fad569410637d2436d5923caf2137942ffdd1
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-11-06 15:57:14 -0300

    Implement installation with python script

---
+++ install.py
@@ -0,0 +1,27 @@
+import os
+
+commands = [
+    'cat mirrorlist > /etc/pacman.d/mirrorlist',
+    'timedatectl set-ntp true',
+    'mkfs.fat -F32 /dev/sda1',
+    'mkfs.ext4 /dev/sda2',
+    'mount /dev/sda2 /mnt',
+    'mkdir /mnt/efi',
+    'mount /dev/sda1 /mnt/efi',
+    'pacstrap /mnt base linux linux-firmware dhcp',
+    'genfstab -U /mnt >> /mnt/etc/fstab',
+    'arch-chroot /mnt ln -sf /usr/share/zoneinfo/Brazil/East /etc/localtime',
+    'arch-chroot /mnt hwclock --systohc',
+    'arch-chroot /mnt echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen',
+    'arch-chroot /mnt locale-gen',
+    'arch-chroot /mnt echo "LANG=en_US.UTF-8" > /etc/locale.conf',
+    'arch-chroot /mnt echo "KEYMAP=br-abnt2" > /etc/vconsole.conf',
+    'arch-chroot /mnt echo "arch" > "/etc/hostname"',
+    'arch-chroot /mnt passwd',
+    'arch-chroot /mnt pacman -S grub efibootmgr',
+    'arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=Arch',
+    'arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg',
+]
+
+for command in commands:
+    os.system(command)
