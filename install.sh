16bf472e553d115438ba783d0204add8f49e13c1
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-11-06 15:07:40 -0300

    Improves chroot commands

--- install.sh
+++ install.sh
@@ -7,16 +7,14 @@ mkdir /mnt/efi
 mount /dev/sda1 /mnt/efi
 pacstrap /mnt base linux linux-firmware dhcp
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt << 'EOF'
-ln -sf /usr/share/zoneinfo/Brazil/East /etc/localtime
-hwclock --systohc
-echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
-echo "arch" > "/etc/hostname"
-passwd
-pacman -S grub efibootmgr
-grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-EOF
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/Brazil/East /etc/localtime
+arch-chroot /mnt hwclock --systohc
+arch-chroot /mnt echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
+arch-chroot /mnt locale-gen
+arch-chroot /mnt echo "LANG=en_US.UTF-8" > /etc/locale.conf
+arch-chroot /mnt echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
+arch-chroot /mnt echo "arch" > "/etc/hostname"
+arch-chroot /mnt passwd
+arch-chroot /mnt pacman -S grub efibootmgr
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
