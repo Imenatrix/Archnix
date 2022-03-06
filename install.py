5b215ba227e66fbf7599b2afce0ea03f56f420d3
Ruan Gomes <ruangomesdorosario@gmail.com>
2022-03-06 10:59:45 -0300

    Change install script to pass execution to another script

--- install.py
+++ install.py
@@ -52,32 +52,12 @@ commands = [
     f'mount {efi} /mnt/efi',
     f'pacstrap /mnt {" ".join(packages)}',
     'genfstab -U /mnt >> /mnt/etc/fstab',
-    f'arch-chroot /mnt ln -sf /usr/share/zoneinfo/{timezone} /etc/localtime',
-    'arch-chroot /mnt hwclock --systohc',
-    f'arch-chroot /mnt bash -c "echo {language}.UTF-8 UTF-8 >> /etc/locale.gen"',
-    'arch-chroot /mnt locale-gen',
-    f'arch-chroot /mnt bash -c "echo LANG={language}.UTF-8 > /etc/locale.conf"',
-    f'arch-chroot /mnt bash -c "echo KEYMAP={keymap} > /etc/vconsole.conf"',
-    f'arch-chroot /mnt bash -c "echo {hostname} > /etc/hostname"',
-    'arch-chroot /mnt passwd',
-    'arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=Arch',
-    'arch-chroot /mnt bash -c "echo GRUB_DISABLE_OS_PROBER=false >> /etc/default/grub"',
-    'arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg',
-    'arch-chroot /mnt pacman -S git base-devel',
-    'arch-chroot /mnt useradd -m -G wheel yay-tmp',
-    'arch-chroot /mnt sed -i "s/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers',
-    'arch-chroot /mnt /usr/bin/runuser -u yay-tmp -- bash -c "cd ~ && git clone https://aur.archlinux.org/yay-bin.git"',
-    'arch-chroot /mnt /usr/bin/runuser -u yay-tmp -- bash -c "cd ~/yay-bin && makepkg -si"',
-    f'arch-chroot /mnt /usr/bin/runuser -u yay-tmp -- yay -S {" ".join(config.packages)}',
-    'arch-chroot /mnt userdel -r yay-tmp',
-    'arch-chroot /mnt sed -i "s/^%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers',
-    'arch-chroot /mnt sed -i "s/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/" /etc/sudoers',
-    f'arch-chroot /mnt systemctl enable {" ".join(services)}',
+    'cp coiso.py /mnt',
+    'cp config.json /mnt',
+    'arch-chroot /mnt python coiso.py'
+    'rm /mnt/coiso.py',
+    'rm /mnt/config.json'
 ]
-
-for user in users:
-    commands.append(f'arch-chroot /mnt useradd -m -G wheel -s /bin/{user.shell} {user.login}')
-    commands.append(f'arch-chroot /mnt passwd {user.login}')
 
 commands += config.postinstall
 
